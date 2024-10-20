1932155f2645d542c3b72ebf71517dd1e75d173e
ugbeadie <ugbeadie3@gmail.com>
2024-10-20 14:19:47 +0100

    from and fromTo method

---
+++ 4. from and fromTo Methods/app.js
@@ -0,0 +1,19 @@
+// gsap.from(".box1", {
+//   y: -200,
+//   duration: 3,
+//   ease: "linear",
+// });
+
+gsap.fromTo(
+  ".box2",
+  { y: 200, opacity: 0 },
+  {
+    opacity: 1,
+    y: -200,
+    duration: 3,
+    ease: "linear",
+    borderRadius: 0,
+    repeat: -1,
+    yoyo: true,
+  }
+);
