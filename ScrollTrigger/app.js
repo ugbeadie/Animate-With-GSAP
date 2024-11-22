7f8c3ee3d8bd9a0f29d551b894c3fb3c72e6464a
ugbeadie <ugbeadie3@gmail.com>
2024-11-22 18:24:05 +0100

    working on hover effects

--- ScrollTrigger/app.js
+++ ScrollTrigger/app.js
@@ -1,18 +1,18 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".square", {
-  x: 400,
-  duration: 8,
-  //   repeat: -1,
-  //   yoyo: true,
-  scrollTrigger: {
-    trigger: ".square",
-    start: "top 80%",
-    end: "top 30%",
-    scrub: 2,
-    pin: true,
-    toggleActions: "restart none none none",
-    markers: true,
-    toggleClass: "red",
-  },
+// gsap.to(".square", {
+//   x: 400,
+//   duration: 8,
+//   //   repeat: -1,
+//   //   yoyo: true,
+ScrollTrigger.create({
+  trigger: ".square",
+  start: "top 80%",
+  end: "top 30%",
+  scrub: 2,
+  pin: true,
+  toggleActions: "restart none none none",
+  markers: true,
+  toggleClass: "red",
+  //   },
 });
